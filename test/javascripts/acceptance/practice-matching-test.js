5cb0a489a31e6c7879125842993c9b456514ffc5
atang-sp <atang-sp@users.noreply.github.com>
2026-07-28 23:14:10 +0800

    feat: make legacy matching read-only

--- test/javascripts/acceptance/practice-matching-test.js
+++ test/javascripts/acceptance/practice-matching-test.js
@@ -11,6 +11,7 @@ acceptance("Practice Matching | maintenance mode", function (needs) {
         practice_interests: [],
         practice_matches: [],
         deprecated: true,
+        read_only: true,
         replacement_url: "/where-is-my-friends/interests",
       })
     );
@@ -23,5 +24,8 @@ acceptance("Practice Matching | maintenance mode", function (needs) {
     assert
       .dom(".practice-matching-deprecation a")
       .hasAttribute("href", "/where-is-my-friends/interests");
+    assert.dom("[data-test-practice-matching-read-only]").exists();
+    assert.dom("[data-test-add-practice-interest]").doesNotExist();
+    assert.dom("[data-test-remove-practice-interest]").doesNotExist();
   });
 });
