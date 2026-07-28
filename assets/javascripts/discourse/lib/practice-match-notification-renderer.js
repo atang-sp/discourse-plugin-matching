0aa59d7edb99d679d7641f8bee7a4cb60f9b3162
atang-sp <217774001+atang-sp@users.noreply.github.com>
2026-07-28 23:52:37 +0800

    fix: preserve custom notification actions (#3)
    Co-authored-by: atang-sp <atang-sp@users.noreply.github.com>

--- assets/javascripts/discourse/lib/practice-match-notification-renderer.js
+++ assets/javascripts/discourse/lib/practice-match-notification-renderer.js
@@ -1,17 +1,11 @@
 import { i18n } from "discourse-i18n";
 
-const PRACTICE_MATCH_MESSAGE =
-  "practice_matching.notification.mutual_match";
-
 export default function practiceMatchNotificationRenderer(
   NotificationTypeBase
 ) {
   return class extends NotificationTypeBase {
     get linkHref() {
-      if (
-        this.notification.data.message === PRACTICE_MATCH_MESSAGE &&
-        this.notification.data.action_url
-      ) {
+      if (this.notification.data.action_url) {
         return this.notification.data.action_url;
       }
 
